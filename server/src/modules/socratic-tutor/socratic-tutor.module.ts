9c472772f915874a4b54b0a94ea3aa7b3d9665db
Nourhan Singer <nourhansingerr7@gmail.com>
2026-08-03 18:15:43 +0300

    feat(topic-state): register TopicState service providers (#163)

--- server/src/modules/socratic-tutor/socratic-tutor.module.ts
+++ server/src/modules/socratic-tutor/socratic-tutor.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common'
 
 import { PrismaModule } from '../prisma/prisma.module'
+import {
+  PrismaTopicStateRepository,
+  TopicStateRepository,
+} from './topic-state.repository'
+import { TopicStateService } from './topic-state.service'
 import { PrismaTopicRepository, TopicRepository } from './topic.repository'
 import { TopicService } from './topic.service'
 
 @Module({
   imports: [PrismaModule],
   providers: [
+    TopicStateService,
     TopicService,
+    {
+      provide: TopicStateRepository,
+      useClass: PrismaTopicStateRepository,
+    },
     {
       provide: TopicRepository,
       useClass: PrismaTopicRepository,
     },
   ],
-  exports: [TopicService],
+  exports: [TopicService, TopicStateService],
 })
 export class SocraticTutorModule {}
